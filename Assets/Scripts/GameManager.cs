9dbbe635c4e002da3b4568ba16bb1128509ea57d
OTWhite04 <owen.t.white2005@gmail.com>
2025-02-03 15:33:22 -0400

    Adding Scene Changes

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public LevelManager levelManager;
+    public GameObject player;
+
+    private void Awake()
+    {
+        #region Singleton Pattern
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+
+        #endregion
+
+
+    }
+
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    private string spawnPointName;
+
+    public void LoadSceneWithSpawnPoint(string sceneName, string spawnPoint)
+    {
+        spawnPointName = spawnPoint;
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+        SceneManager.LoadScene(sceneName);
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        SetPlayerToSpawn(spawnPointName);
+
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    public void SetPlayerToSpawn(string spawnPointName)
+    {
+        GameObject spawnPointObject = GameObject.Find(spawnPointName);
+
+        if (spawnPointObject != null)
+        {
+            Transform spawnPointTransform = spawnPointObject.transform;
+            GameManager.Instance.player.transform.position = spawnPointTransform.position;
+        }
+        else
+        {
+            Debug.Log($"Error, {spawnPointName} not found in scene!");
+        }
+    }
+
+}
+
